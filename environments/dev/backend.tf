e9875f128140ca485a6f52d5519293e671c41b4e
nassprasad <nassprasad@gmail.com>
2026-05-11 15:32:18 +0530

    update2-deploy-demo.yml

--- environments/dev/backend.tf
+++ environments/dev/backend.tf
@@ -1,17 +1,9 @@
 terraform {
-
   backend "s3" {
-
     bucket = "demo-infra-backend-mumbai"
-
-    key    = "dev/terraform.tfstate"
-
+    key    = "dev/mumbai/terraform.tfstate"
     region = "ap-south-1"
-
     encrypt = true
-
     use_lockfile = true
-
   }
-
 }
