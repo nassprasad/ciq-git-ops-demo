5b775b7dcb3891760f51cc8c24c64cdd5d4db115
nassprasad <nassprasad@gmail.com>
2026-05-11 17:01:50 +0530

    Added Terraform workflow for vpc

--- environments/dev/provider.tf
+++ environments/dev/provider.tf
@@ -1,5 +1,6 @@
 provider "aws" {
-  region = "ap-south-1"
+  region = var.aws_region
+  #region = "ap-south-1"
   # $env:AWS_PROFILE="terraform-role"  --> must run from CLI
   profile = "terraform-role"
    
