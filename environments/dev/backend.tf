736b640c17000943abca4f7812af34a92e36f173
nassprasad <nassprasad@gmail.com>
2026-05-12 00:02:06 +0530

    Delete environments directory

--- environments/dev/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket = "demo-infra-backend-mumbai"
-    key    = "dev/mumbai/terraform.tfstate"
-    region = "ap-south-1"
-    encrypt = true
-    use_lockfile = true
-  }
-}
--- environments/dev/provider.tf
+++
@@ -1,7 +0,0 @@
-provider "aws" {
-  region = var.aws_region
-  #region = "ap-south-1"
-  # $env:AWS_PROFILE="terraform-role"  --> must run from CLI
-  profile = "terraform-role"
-   
-}
